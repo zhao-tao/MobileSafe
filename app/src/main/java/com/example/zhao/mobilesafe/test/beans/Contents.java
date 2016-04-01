2c82d618f0eb9b5cbc86421b92c159fa6a26f03c
zhaotao <zhao-tao@msn.com>
2016-04-01 09:49:31 +0800

    分包之后，导包出问题，重新修改导包

--- app/src/main/java/com/example/zhao/mobilesafe/test/beans/Contents.java
+++
@@ -1,9 +0,0 @@
-package com.example.zhao.mobilesafe.test.beans;
-
-/**
- * Created by zhao on 2016/3/31.
- */
-public class Contents {
-    public static final String SPLASH_IMG = "http://image.juntu.com/uploadfile/2015/1115/20151115014041861.jpg";
-    public static final String UP_DATA = "http://p-www.juntu.com/index.php?m=app&c=index&a=mobile_update_port&id=2";
-}
--- app/src/main/java/com/example/zhao/mobilesafe/test/beans/UpDate.java
+++
@@ -1,7 +0,0 @@
-package com.example.zhao.mobilesafe.test.beans;
-
-/**
- * Created by zhao on 2016/3/31.
- */
-public class UpDate {
-}
